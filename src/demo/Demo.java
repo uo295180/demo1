86083d6a37d082f0a7f314fd77f6def5ff44a104
orvizz <153991700+orvizz@users.noreply.github.com>
2024-09-24 22:36:25 +0200

    Changes in the existent file

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -1,5 +1,9 @@
 package demo;
 
 public class Demo {
-
+	public static void main(String[] args) {
+		System.out.println("line 1");
+		System.out.println("line 2");
+		System.out.println("line 3");
+	}
 }
